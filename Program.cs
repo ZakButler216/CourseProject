72eeee2777f03cf430a37344551b4be3e6cdf8f1
ZakButler216 <zakbutler.zb@gmail.com>
2024-04-09 16:36:30 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int success = 0;
+int failure = 0;
+int counter = 0;
+
+while (counter < 5)
+{
+    Console.WriteLine("Pleae enter an age:");
+    int age = int.Parse(Console.ReadLine());
+
+    if (age > 18)
+    {
+        success++;
+        Console.WriteLine(age + ": Entry");
+    }
+    if (age >= 0 && age <= 18)
+    {
+        failure++;
+        Console.WriteLine(age + ": No Entry");
+    }
+    counter++;
+}
+
+double x = success;
+double y = counter;
+double z = x / y;
+int percent = (int)(z * 100);
+
+Console.WriteLine(success + " Successed");
+Console.WriteLine(failure + " Failed");
+Console.WriteLine(percent + "% Gained Entry");
